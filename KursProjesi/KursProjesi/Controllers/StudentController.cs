d0d0caa28c4a1f9f9e7b1091673624c577b0c864
ibrahimayaz <ayazibrahim13@gmail.com>
2025-03-05 16:25:29 +0300

    Hafta02-KursProjesi

---
+++ KursProjesi/KursProjesi/Controllers/StudentController.cs
@@ -0,0 +1,25 @@
+﻿using KursProjesi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KursProjesi.Controllers
+{
+    public class StudentController : Controller
+    {
+        public static List<Student> students = new();
+
+        public StudentController() { }
+
+        
+        public IActionResult Index() { 
+            return View(); 
+        }
+
+        [HttpPost]
+        public IActionResult Index(Student s) { 
+
+            students.Add(s);
+            return View("List", students); 
+        }
+
+    }
+}
---
+++ KursProjesi/KursProjesi/Models/Student.cs
@@ -0,0 +1,28 @@
+﻿namespace KursProjesi.Models
+{
+    public class Student
+    {
+        public Student()
+        {
+            EnrolDate = DateTime.Now;
+        }
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+
+        public string Fullname
+        {
+            get
+            {
+                return Firstname + " " + Lastname;
+            }
+        }
+
+        public string SelectedCourse { get; set; }
+
+        public int Age { get; set; }
+
+        public DateTime EnrolDate { get; set; }
+
+    }
+}
