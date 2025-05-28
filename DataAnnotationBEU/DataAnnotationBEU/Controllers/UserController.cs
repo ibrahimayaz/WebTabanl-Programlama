e8e0d142f21d16abf780a5b57cfb2e9a447f8904
unknown <ayazibrahim13@gmail.com>
2025-05-28 15:49:42 +0300

    Hafta14

---
+++ DataAnnotationBEU/DataAnnotationBEU/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using DataAnnotationBEU.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DataAnnotationBEU.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(UserVM userVm)
+        {
+            if (ModelState.IsValid)
+            {
+                //Problem yok kaydı gerçekleştir.
+                return View();
+            }
+            else
+            {
+                return View(userVm);
+            }
+        }
+    }
+}
---
+++ DataAnnotationBEU/DataAnnotationBEU/ViewModels/UserVM.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DataAnnotationBEU.ViewModels
+{
+    public class UserVM
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Lütfen belirtirilen aralıkta adınızı giriniz.")]
+        public string Name { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Lütfen belirtirilen aralıkta adınızı giriniz.")]
+        public string Surname { get; set; }
+        [Required]
+        [Range(18, 65, ErrorMessage = "Üzgünüm yaşınız belirtilen aralıkta değil.")]
+        public string Age { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage ="Şifre alanları uyuşmuyor")]
+        public string ConfirmPassword { get; set; }
+
+        [Required(ErrorMessage = "Bu alan zorunludur.")]
+        [EmailAddress(ErrorMessage ="E-mail adresi doğru biçimde değil.")]
+        public string Email { get; set; }
+    }
+}
